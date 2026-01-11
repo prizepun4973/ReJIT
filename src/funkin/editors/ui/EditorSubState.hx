f5af420731dca5ef747e788adc7afd5a2899bdcc
silent-crystal <3673661750@qq.com>
2026-01-11 16:05:33 +0800

    help

--- src/funkin/editors/ui/EditorSubState.hx
+++ src/funkin/editors/ui/EditorSubState.hx
@@ -4,26 +4,25 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 import funkin.jit.InjectedSubState;
 import flixel.util.FlxColor;
 import funkin.component.ui.*;
-import flixel.FlxSprite;
-import flixel.FlxG;
 
 import lime.ui.*;
 
 class EditorSubState extends funkin.jit.InjectedSubState {
-    public var widgets:FlxTypedGroup<FlxSprite> = new FlxTypedGroup<FlxSprite>();
+    var textBoxs:Array<TextBoxWidget> = [];
 
     override public function new(script:String) {
         super(script);
-        add(widgets);
-        FlxG.stage.window.onKeyDown.add(onKeyDown);
+        flixel.FlxG.stage.window.onKeyDown.add(onKeyDown);
     }
 
     public function addButton(X:Float, Y:Float, text:String, onClick:Void -> Void) {
-        widgets.add(new ButtonWidget(this, X, Y, text, onClick));
+        add(new ButtonWidget(this, X, Y, text, onClick));
     }
 
     public function addTextBox(X:Float, Y:Float, width:Int, buttonText:String, onChange:TextBoxWidget -> Void) {
-        widgets.add(new TextBoxWidget(this, X, Y, width, buttonText, onChange));
+        var textBox:TextBoxWidget = new TextBoxWidget(this, X, Y, width, buttonText, onChange);
+        textBoxs.push(textBox);
+        add(textBox);
     }
 
     public function onKeyDown(key:KeyCode, modifier:KeyModifier) {
@@ -32,12 +31,9 @@ class EditorSubState extends funkin.jit.InjectedSubState {
                 return;
             case ESCAPE:
                 var canExit:Bool = false;
-                widgets.forEachAlive(function (widget:FlxSprite) {
-                    if (Std.isOfType(widget, TextBoxWidget)) {
-                        var textBox:TextBoxWidget = cast (widget, TextBoxWidget);
-                        if (!textBox.editing) canExit = true;
-                    }
-                });
+                for (textBox in textBoxs) {
+                    if (!textBox.editing) canExit = true;
+                }
                 if (canExit) close();
         }
     }
